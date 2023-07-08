9cfe0d25954b01d1dfd6116547ebaa0bd5f8dc76
Edgar Landivar <elandivar@yubox.com>
2023-07-08 18:35:44 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# UI-Forge
+
+UI-Forge is a tool designed to streamline the setup process for initiating any Laravel-based admin interface development project. It utilizes open-admin on Laravel and neatly packages it all in a Docker microservices architecture.
+
+Within minutes, a developer can start working on a clean interface for their Laravel-based application.
+
+UI-Forge is compatible across all operating systems (Windows, Linux, macOS) that have Docker installed.
+
+## Requirements
+
+Before running UI-Forge, ensure that your system meets the following requirements:
+
+1. **Python:** UI-Forge is a Python script, so you need Python installed in your system. Check if you have Python using the command: `python --version`. If you don't have Python installed, you can download it from the [Python official website](https://www.python.org/).
+
+2. **Docker Compose:** The tool uses Docker Compose to manage Docker applications. Check if you have Docker Compose installed using the command: `docker-compose --version`. If you don't have Docker Compose installed, you can download it from the [Docker official website](https://docs.docker.com/compose/install/).
+
+## Installation
+
+To use UI-Forge, follow the steps below:
+
+1. Clone the repository to your local machine or download the `install.py` script.
+2. Navigate to the directory containing the `install.py` file in your terminal.
+3. Run the command: `python install.py`
+
+Follow the instructions provided by the script to set up your Laravel-based admin interface.
+
+## Documentation
+
+For more detailed information, consider exploring the following resources:
+
+- [Docker Compose Documentation](https://docs.docker.com/compose/)
+
+## Contributions
+
+Contributions, issues, and feature requests are welcome. Feel free to check issues page if you want to contribute.
+
+## License
+
+This project is licensed under the MIT License.
+
+Happy Coding!
+
---
+++ install.py
@@ -0,0 +1,97 @@
+import os
+import re
+import subprocess
+import shutil
+import sys
+
+def check_docker_installed():
+    try:
+        subprocess.check_output(["docker-compose", "--version"])
+    except FileNotFoundError:
+        print("docker-compose is not installed. Please install it and try again.")
+        sys.exit(1)
+
+def check_files_exist():
+    if not (os.path.isfile("./Dockerfile") or os.path.isfile("./docker-compose.yml")):
+        print("Dockerfile or docker-compose.yml not found. Please make sure either of them is present in the current directory.")
+        sys.exit(1)
+
+def get_input(param_name, default_value, required=False, length_limit=16):
+    while True:
+        try:
+            value = input(f"Please enter {param_name} ({default_value if not required else 'mandatory'}): ")
+            value = value or default_value
+            if not value and required:
+                raise ValueError(f"{param_name} is a required parameter. Please provide a value.")
+            if length_limit and (len(value) > length_limit or not re.match("^[a-zA-Z0-9_]+$", value)):
+                raise ValueError(f"Invalid {param_name}. Please make sure it is not longer than {length_limit} characters and contains only alphanumeric characters and underscores.")
+            return value
+        except ValueError as e:
+            print(str(e))
+
+def replace_macros_in_file(db_name, db_user, db_pass):
+    if os.path.isfile("./.env"):
+        overwrite = input("File .env already exists. Do you want to overwrite it? (yes/no): ")
+        if overwrite.lower() != "yes":
+            print("Leaving existing .env file.")
+            return
+    print("Generating .env file...")
+    with open("env.tpl", "r") as file:
+        file_data = file.read()
+    file_data = file_data.replace("{DB_DATABASE}", db_name)
+    file_data = file_data.replace("{DB_USERNAME}", db_user)
+    file_data = file_data.replace("{DB_PASSWORD}", db_pass)
+    with open(".env", "w") as file:
+        file.write(file_data)
+
+def check_laravel_directory():
+    if os.path.isdir("./laravel"):
+        print("Warning: Deleting the 'laravel' directory could result in data loss. Please make sure you have a backup before proceeding.")
+        delete = input("Directory 'laravel' already exists. Do you want to delete it? (yes/no): ")
+        if delete.lower() != "yes":
+            print("Leaving existing 'laravel' directory. Execution stopped.")
+            sys.exit(1)
+        else:
+            print("Deleting 'laravel' directory...")
+            shutil.rmtree('./laravel')
+
+def run_commands():
+    commands = [
+        "docker-compose build app",
+        "mkdir laravel",
+        "docker-compose up -d",
+        "docker-compose exec app composer create-project --prefer-dist laravel/laravel .",
+        "rm ./laravel/.env",
+        "cp .env ./laravel/",
+        "docker-compose exec app composer require open-admin-org/open-admin",
+        "docker-compose exec app php artisan vendor:publish --provider=\"OpenAdmin\Admin\AdminServiceProvider\"",
+        "docker-compose exec app php artisan admin:install",
+        "docker-compose exec app php artisan key:generate",
+    ]
+    for command in commands:
+        print(f"Running command: {command}")
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        while True:
+            line = p.stdout.readline()
+            if not line:
+                break
+            print(line.decode('utf-8').strip())
+        p.wait()
+        if p.returncode != 0:
+            print(f"An error occurred while running command '{command}'.")
+            sys.exit(1)
+    print("All commands executed successfully.")
+
+def main():
+    check_docker_installed()
+    check_files_exist()
+    db_name = get_input("database name", "boxera")
+    db_user = get_input("database user name", "boxera")
+    db_pass = get_input("database user password", "", required=True, length_limit=None)
+    replace_macros_in_file(db_name, db_user, db_pass)
+    check_laravel_directory()
+    run_commands()
+
+if __name__ == "__main__":
+    main()
+
